40ab1dac5636ef901bf1639e4733b29266c773a3
neeli <neelimareddygopu1@gmail.com>
2025-01-06 12:40:16 +0530

    Roboshop frontend script

--- print.sh
+++ print.sh
@@ -12,5 +12,16 @@ blue 34
 magenta 35
 cyan 36
 
-echo -e "\e[32m Hello in red color\e[om"
+echo -e "\e[33m Hello in red color\e[0m"
+
+input to a file <
+output > or 1>
+
+error  2>
+
+error and output === &>
+
+> overwrite the content
+>> append the content
+>/dev/null  nullify the output that goes to this file
 
