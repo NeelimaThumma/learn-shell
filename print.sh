ee4fcf2138d4026e742747ee9b334d5249b91e3f
neeli <neelimareddygopu1@gmail.com>
2025-01-06 11:53:03 +0530

    List of Topics updated for Shell Scripting

---
+++ print.sh
@@ -0,0 +1,15 @@
+echo Hello World
+echo -e "\e[COLmMessage\e[0m"
+
+-e enable colors
+
+COL Color
+
+red 31,green 32
+yellow 33
+blue 34
+magenta 35
+cyan 36
+
+echo -e "\e[32m Hello in red color\e[om"
+
